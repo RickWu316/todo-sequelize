c20dbd64776e237895c6353501d34f61b19d563d
Rick Wu <rickwu316@gmail.com>
2021-02-02 03:20:06 +0800

    feat: add fb login

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -27,7 +27,6 @@ router.get('/:id/edit', (req, res) => {
     const userId = req.user.id
     const id = req.params.id
     return Todo.findOne({ where: { id, userId } })
-        // .lean()
         .then(todo => res.render('edit', { todo: todo.toJSON() }))
         .catch(error => console.log(error))
 })
