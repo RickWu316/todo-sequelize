6a841561c9a442b448c34b8f99d3106716e1147b
Rick Wu <rickwu316@gmail.com>
2021-02-02 02:39:07 +0800

    add user authentication

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -11,4 +11,46 @@ router.get('/:id', (req, res) => {
         .catch(error => console.log(error))
 })
 
+
+// router.get('/new', (req, res) => {
+//     return res.render('new')
+// })
+// router.post('/', (req, res) => {
+//     const userId = req.user._id
+//     const name = req.body.name
+//     return Todo.create({ name, userId })
+//         .then(() => res.redirect('/'))
+//         .catch(error => console.log(error))
+// })
+
+// router.get('/:id/edit', (req, res) => {
+//     const userId = req.user.id
+//     const id = req.params.id
+//     return Todo.findOne({ where: { id, userId } })
+//         .lean()
+//         .then(todo => res.render('edit', { todo }))
+//         .catch(error => console.log(error))
+// })
+// router.put('/:id', (req, res) => {
+//     const userId = req.user.id
+//     const id = req.params.id
+//     const { name, isDone } = req.body
+//     return Todo.findOne({ where: { id, userId } })
+//         .then(todo => {
+//             todo.name = name
+//             todo.isDone = isDone === 'on'
+//             return todo.save()
+//         })
+//         .then(() => res.redirect(`/todos/${id}`))
+//         .catch(error => console.log(error))
+// })
+// router.delete('/:id', (req, res) => {
+//     const userId = req.user.id
+//     const id = req.params.id
+//     return Todo.findOne({ where: { id, userId } })
+//         .then(todo => todo.remove())
+//         .then(() => res.redirect('/'))
+//         .catch(error => console.log(error))
+// })
+
 module.exports = router
